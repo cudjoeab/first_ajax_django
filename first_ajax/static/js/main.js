91f81403dc34abcd1859bbbfdb0157f4f7bbe9ff
cudjoeab <cudjoeab@gmail.com>
2019-08-22 11:38:55 -0400

    adding time button, and the ajax request with params in axios

--- first_ajax/static/js/main.js
+++ first_ajax/static/js/main.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", function() {
     let rootButton = document.getElementById('request-root');
     let pingButton = document.getElementById('ping-button');
     let countButton = document.getElementById('count-button'); 
+    let timeButton = document.getElementById('time-button');
     // creating a section in HTML 
     let section = document.createElement('section');
     let body = document.querySelector('body'); 
@@ -47,6 +48,31 @@ document.addEventListener("DOMContentLoaded", function() {
                 const countText = document.createElement('h2')
                 countText.innerHTML = response.data    
                 section.appendChild(countText)
+             })
+             .catch(function(error){
+                 const errorText = document.createElement('h2')
+                 errorText.innerHTML = "Ouf... There seems to be an error. Sorry!"
+                 section.appendChild(errorText) 
+             })
+             .then(function(response){
+                 const endText = document.createElement('h2')
+                 endText.innerHTML = "That's all for now folks!"
+                 section.appendChild(endText)
+             });
+
+
+    });
+
+    timeButton.addEventListener('click', () => {
+        let request = 
+        axios.get('http://intro-ajax-api.herokuapp.com/time', {
+            params: { timezone : 'Asia/Kolkata'}
+        })
+             .then(function(response){
+                console.log(response.data)
+                const timeText = document.createElement('h2')
+                timeText.innerHTML = response.data    
+                section.appendChild(timeText)
              })
              .catch(function(error){
                  const errorText = document.createElement('h2')
