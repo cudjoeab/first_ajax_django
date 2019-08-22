0002456deefef712b2d9e347f4a3603b506ad259
cudjoeab <cudjoeab@gmail.com>
2019-08-22 00:07:00 -0400

    adding root button, ping/pong button and ping text

--- first_ajax/static/js/main.js
+++ first_ajax/static/js/main.js
@@ -1,3 +1,33 @@
 document.addEventListener("DOMContentLoaded", function() {
-  
+    
+    let rootButton = document.getElementById('request-root')
+    let pingButton = document.getElementById('ping-button')
+    // creating a section in HTML 
+    let section = document.createElement('section')
+    let body = document.querySelector('body')
+    body.appendChild(section)
+
+
+    rootButton.addEventListener('click', () => {
+        let request = axios.get('http://intro-ajax-api.herokuapp.com/')
+                           .then(function(response){
+        console.log('-- request received --')
+        }); 
+    });
+
+    pingButton.addEventListener('click', () => {
+        let request = 
+        axios.get('http://intro-ajax-api.herokuapp.com/ping')
+             .then(function(response){
+                console.log(response.data)
+                const pingText = document.createElement('h2')
+                pingText.innerHTML = response.data    
+                section.appendChild(pingText)
+             })
+
+
+    });
+    
+
+    
 });
